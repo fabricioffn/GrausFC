69a3456244fa4266767debbb80fd32866ab55810
fabricio <fnmamao@gmail.com>
2020-05-22 10:51:20 -0300

    adiciona conversor

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double F;
+            Double C;
+            Console.WriteLine("Conversor de fahrenheit para celsius");
+            Console.WriteLine("informe a tempertatura em fahrenheit :");
+            F = Convert.ToDouble(Console.ReadLine());
+
+            // conversão
+
+            C = (F - 32) * 5/9;
+            Console.WriteLine("a temperatura em Celsius é : " + C);
+        }
+    }
+}
